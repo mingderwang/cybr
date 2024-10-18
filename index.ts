37d6a4ae0f1c7dbd3dc4747479b4fc7e0e86854f
mingderwang <mingderwang@me.com>
2024-10-18 21:59:02 +0800

    test v0.0.2

--- index.ts
+++ index.ts
@@ -1,2 +1,53 @@
 #!/usr/bin/env node
-console.log("cybr!");
+import { createCommand } from 'commander'
+import figlet from 'figlet'
+import data from "./package.json" with { type: "json" };
+
+const program = createCommand(data.name)
+program
+  .option('-p, --pizza-type <TYPE>', 'type of pizza') 
+  .option('-c, --cheese [CHEESE]', 'add extra cheese', 'mozzarella') 
+  .option('-t, --tip <VALUE>', 'add tip to purchase cost', parseFloat) 
+
+program
+  .description(data.description)
+  .version(data.version)
+  .command('farewell <name>')
+  .description('Say farewell to a user')
+  .action((name) => {
+    console.log(`Goodbye, ${name}!`);
+  })
+
+program
+  .command('clone <source> [destination]')
+  .action((source, destination) => {
+    console.log('clone command called')
+  })
+
+program
+  .addHelpText('beforeAll', 
+    '\n' + 
+    await figlet.text('cybr',
+      {
+        font: "Ghost",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+      },
+    )
+    + '\n'
+  )
+  .addHelpText('after', '🥭	🍧🧋🍻💭 ---------------')
+  .addHelpText('afterAll', 
+  `
+Examples:
+  cybr clone --help
+  cybr help clone
+  
+`,
+  )
+  .showSuggestionAfterError(true)
+
+program
+  .parse(process.argv)
